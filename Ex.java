12bbda915325312dc5a87c42eb2fa4078f22c78d
syc <tlsdydcks135@naver.com>
2023-01-26 17:40:46 +0900

    ex2_6

--- Ex.java
+++ Ex.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
-//ex2_4 초를 입력하면 시간, 분, 초로 환산해 출력하는 프로그램
+//ex2_6 화씨온도를 섭씨온도로 환산해 출력하는 프로그램
 public class Ex {
 
 	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	System.out.print("초 단위 정수를 입력하세요 : ");
-	int sec = sc.nextInt();
-	int hour = sec/3600;
-	int min = (sec-(hour*3600))/60;
-	sec = sec-(hour*3600)-(min*60);
-	System.out.printf("%d시간 %d분 %d초", hour, min, sec);
+	System.out.print("화씨온도를 입력하세요 : ");
+	double f = sc.nextDouble();
+	double c = (5.0/9.0)*(f-32);
+	
+	System.out.printf("화씨온도 %.2f는 섭씨온도 %.2f", f, c);
 
 	}
 
