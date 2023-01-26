9551ba0b31c3e8980cb120c5ac1fd72f245e0cd4
syc <tlsdydcks135@naver.com>
2023-01-26 17:27:10 +0900

    ex2_3

--- Ex.java
+++ Ex.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
-//ex2
+//ex2_3
 public class Ex {
 
 	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	System.out.print("정수를 입력하세요 : ");
-	int num = sc.nextInt();
-	System.out.printf("%d의 제곱은 %d",num,num*num);
+	System.out.print("원기둥의 밑면 반지름은? ");
+	double r = sc.nextInt();
+	System.out.print("원기둥의 높이는? ");
+	double h = sc.nextInt();
+	System.out.printf("원기둥의 부피는 %.1f",3.14*r*r*h);
 
 	}
 
