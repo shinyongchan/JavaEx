3461984aa1fd321d0a922c5bdc31e4aaf2ec11a6
syc <tlsdydcks135@naver.com>
2023-01-26 17:33:28 +0900

    ex2_4

--- Ex.java
+++ Ex.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
-//ex2_3
+//ex2_4 초를 입력하면 시간, 분, 초로 환산해 출력하는 프로그램
 public class Ex {
 
 	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	System.out.print("원기둥의 밑면 반지름은? ");
-	double r = sc.nextInt();
-	System.out.print("원기둥의 높이는? ");
-	double h = sc.nextInt();
-	System.out.printf("원기둥의 부피는 %.1f",3.14*r*r*h);
+	System.out.print("초 단위 정수를 입력하세요 : ");
+	int sec = sc.nextInt();
+	int hour = sec/3600;
+	int min = (sec-(hour*3600))/60;
+	sec = sec-(hour*3600)-(min*60);
+	System.out.printf("%d시간 %d분 %d초", hour, min, sec);
 
 	}
 
