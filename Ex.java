1f4563b2ba821fae882b37033358ae2d14a9a176
syc <tlsdydcks135@naver.com>
2023-01-26 17:47:11 +0900

    ex2_8

--- Ex.java
+++ Ex.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-//ex2_6 화씨온도를 섭씨온도로 환산해 출력하는 프로그램
+//ex2_8 0부터 999 사이의 정수를 입력받아 각 자릿수를 더한 결과를 출력하는 프로그램
 public class Ex {
 
 	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	System.out.print("화씨온도를 입력하세요 : ");
-	double f = sc.nextDouble();
-	double c = (5.0/9.0)*(f-32);
+	System.out.print("0~999 사이의 숫자를 입력하세요 : ");
+	int num = sc.nextInt();
+	int sum = num/100 + (num%100)/10 + num%10;
 	
-	System.out.printf("화씨온도 %.2f는 섭씨온도 %.2f", f, c);
+	System.out.printf("각 자릿수의 합 = %d", sum );
 
 	}
 
