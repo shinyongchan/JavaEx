37c73e8949cb34d512bb39ceffd0f7da575917b6
syc <tlsdydcks135@naver.com>
2023-01-26 17:24:30 +0900

    ex2_2

--- Ex.java
+++ Ex.java
@@ -1,13 +1,13 @@
-//ex1
+import java.util.Scanner;
+
+//ex2
 public class Ex {
 
 	public static void main(String[] args) {
-		System.out.println("     *");
-		System.out.println("    *** ");
-		System.out.println("   *****   ");
-		System.out.println("  *******  ");
-		System.out.println(" ********* ");
-		System.out.println("***********");
+	Scanner sc = new Scanner(System.in);
+	System.out.print("정수를 입력하세요 : ");
+	int num = sc.nextInt();
+	System.out.printf("%d의 제곱은 %d",num,num*num);
 
 	}
 
