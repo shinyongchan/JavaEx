dc77bea44a0a741f0bfb41833d8f80c1ab172948
syc <tlsdydcks135@naver.com>
2023-01-26 17:58:42 +0900

    ex2_9

--- Ex.java
+++ Ex.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
-//ex2_8 0부터 999 사이의 정수를 입력받아 각 자릿수를 더한 결과를 출력하는 프로그램
+//ex2_9 
 public class Ex {
 
 	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	System.out.print("0~999 사이의 숫자를 입력하세요 : ");
-	int num = sc.nextInt();
-	int sum = num/100 + (num%100)/10 + num%10;
-	
-	System.out.printf("각 자릿수의 합 = %d", sum );
+	System.out.print("전공 이수학점 : ");
+	int a = sc.nextInt();
+	System.out.print("교양 이수학점 : ");
+	int b = sc.nextInt();
+	System.out.print("일반 이수학점 : ");
+	int c = sc.nextInt();
+	String grad = ((a + b + c>=140)&&(a>=70)&&((b>=30 && c>=30)||(b+c>=80)))?"졸업 가능":"졸업 불가능";
+	System.out.printf("%s", grad );
 
 	}
 
