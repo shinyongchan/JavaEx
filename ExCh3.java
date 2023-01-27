ee6e525184865726a3fb06202372576e08266df0
syc <tlsdydcks135@naver.com>
2023-01-27 18:31:40 +0900

    ex3_4

--- ExCh3.java
+++ ExCh3.java
@@ -4,18 +4,16 @@ import java.util.Scanner;
 public class ExCh3 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int sum = 0;
-		do
+		for (int j = 1; j <= 5; j++)
 		{
-			System.out.print("양의 정수를 입력하시오 : ");
-			int num = sc.nextInt();
-			if (num % 2 == 0)
-				sum = sum + num;
-			else if (num <= 0)
-				break;
-		} while (true);
-		System.out.printf("입력한 양의 정수 중에서 짝수의 합은 %d", sum);
+			for (int i = 1; i <= j; i++)
+			{
+				System.out.print("*");
+			}
+				System.out.println();		
+		}
+		
+		
 	}
 
 }
