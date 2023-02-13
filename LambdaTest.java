04ea3dca9a6f0f8f6b3c2c5e6049bd0170fe6bdf
syc <tlsdydcks135@naver.com>
2023-02-13 18:38:10 +0900

    lambdatest3

--- LambdaTest.java
+++ LambdaTest.java
@@ -1,39 +1,36 @@
 package generictest;
-// Lambda Expression(람다식)
-// - 익명 메서드 (일반 메서드와 달리 이름이 없다)
-// - 메서드와 달리 특정 클래스에 종속되지 않지만, 매개변수, 리턴타입 , 본체를 가진다
-// - 예외처리가 가능하다
-// - 메서드의 인수로 전달될 수도 있고 변수에 대입될 수 있다
-// - 익명 구현 객체와 달리 메서드의 핵심 부분만 포함한다
-// - 실행문이 하나면 중괄호와 세미콜론 생력할 수 있다
-// - 실행문이 하나의 리턴 구문이면 return 키워드도 생략가능
+// x -> Integer.parseInt(x)  람다 표현식
+// Integer :: parseInt       method reference
 
+// 정적 메서드 참조
+// 클래스이름::정적메서드
+// 인스턴스 메서드 참조
+// 객체 이름::인스턴스메서드
+// 생성자 참조
+// 클래스이름::new
+// 배열타입 이름::new
 
-// (타입 매개변수 ....) -> {실행문; 실행문; ....}
-
-interface Postive {
-    int pos(int x);
+interface Calc {
+    double calculate(double d);
 }
-interface Prints {
-    void print();
+
+interface Gets{
+    char get(String s, int i);
 }
 
 public class LambdaTest {
     public static void main(String[] args) {
-        Postive po;
-        Prints pr;
+        Calc c;
+        Gets g;
 
-        po = (int n) -> {return n +1;};
-        po = (n) -> {return n+1;};
-        po = n -> {return n+1;};
-        po = (int n) -> n+1;
+        c = d -> Math.abs(d);
+        System.out.println(c.calculate(-9.9));
+        c = Math::abs;
+        System.out.println(c.calculate(-79));
 
-        pr = () -> {
-            System.out.println("hi");
-        };
-        pr = () -> System.out.println("hi");
-        pr.print();
-
-
+        g = (a, b) -> a.charAt(b);
+        System.out.println(g.get("hello", 2));
+        g = String::charAt;
+        System.out.println(g.get("hello", 2));
     }
 }
